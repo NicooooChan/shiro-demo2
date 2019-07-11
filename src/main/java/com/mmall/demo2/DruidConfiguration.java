95d361562c1db639135d7426bff2a41de74f62e6
chenjizhou <694859042@qq.com>
2019-07-11 19:07:29 +0800

    4-5

--- src/main/java/com/mmall/demo2/DruidConfiguration.java
+++ src/main/java/com/mmall/demo2/DruidConfiguration.java
@@ -39,6 +39,7 @@ public class DruidConfiguration {
         FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new WebStatFilter());
         //添加过滤规则.
         filterRegistrationBean.addUrlPatterns("/*");
+
         //添加不需要忽略的格式信息.
         filterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
         return filterRegistrationBean;
